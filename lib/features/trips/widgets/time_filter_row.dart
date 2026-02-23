d8e7a1b31084968f1169585ddfd4e2d22fdca10b
Buddika Kasun <buddikakasun80@gmail.com>
2026-02-23 20:21:59 +0530

    Time filter UI update

--- lib/features/trips/widgets/time_filter_row.dart
+++ lib/features/trips/widgets/time_filter_row.dart
@@ -25,32 +25,45 @@ class TimeFilterRow extends StatelessWidget {
     ];
 
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      padding: const EdgeInsets.fromLTRB( 12, 8, 12, 4), 
       color: Colors.black,
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: filters.map((filter) {
-          final isSelected = currentFilter == filter['value'];
-          return GestureDetector(
-            onTap: () => onFilterSelected(filter['value'] as String),
-            child: Container(
-              padding: const EdgeInsets.symmetric(vertical: 8),
-              width: 70,
-              decoration: BoxDecoration(
-                color: isSelected ? activeColor : Colors.grey[900],
-                borderRadius: BorderRadius.circular(10),
-              ),
-              alignment: Alignment.center,
-              child: Text(
-                filter['label'] as String,
-                style: TextStyle(
-                  color: isSelected ? Colors.black : Colors.white,
-                  fontWeight: isSelected ? FontWeight.bold : FontWeight.w500,
-                ),
-              ),
+          return Expanded(
+            child: _buildFilterButton(
+              label: filter['label'] as String,
+              value: filter['value'] as String,
+              isSelected: currentFilter == filter['value'],
             ),
           );
         }).toList(),
+      ),
+    );
+  }
+
+  Widget _buildFilterButton({
+    required String label,
+    required String value,
+    required bool isSelected,
+  }) {
+    return GestureDetector(
+      onTap: () => onFilterSelected(value),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 4),
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        decoration: BoxDecoration(
+          color: isSelected ? activeColor : Colors.grey[900],
+          borderRadius: BorderRadius.circular(10),
+        ),
+        alignment: Alignment.center,
+        child: Text(
+          label,
+          style: TextStyle(
+            color: isSelected ? Colors.black : Colors.white,
+            fontWeight: isSelected ? FontWeight.bold : FontWeight.w500,
+            fontSize: 14,
+          ),
+        ),
       ),
     );
   }
