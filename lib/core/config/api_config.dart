3197d7e79d6e424d54727b8ece2aab05e36399e4
Buddika Kasun <buddikakasun80@gmail.com>
2026-06-22 21:01:06 +0530

    2026/06/22 Update version to 1.10.0+23 for release

--- lib/core/config/api_config.dart
+++ lib/core/config/api_config.dart
@@ -16,8 +16,8 @@ class ApiConfig {
         //   // For web (Railway), load from hardcoded config
         //   _loadWebConfig();                         // Prod
         // }
-        // _loadWebConfig();                         // Prod
-        await dotenv.load(fileName: "assets/.env"); // Dev
+        _loadWebConfig();                         // Prod
+        // await dotenv.load(fileName: "assets/.env"); // Dev
       } else {
         // For mobile/desktop, load from .env file
         await dotenv.load(fileName: "assets/.env");
